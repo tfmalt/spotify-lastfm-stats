5620d70a1bfccb40841371c9e3b2129d2c45625b
Thomas Malt <thomas@malt.no>
2014-12-01 20:23:56 +0100

    added config file to repo

---
+++ config.js
@@ -0,0 +1,12 @@
+exports.redis = {
+    host: "localhost",
+    port: 6379,
+    options: {
+        auth_pass: null
+    }
+};
+
+exports.lastfm = {
+    api_key: "<api-key>",
+    user: "<user>"
+};
